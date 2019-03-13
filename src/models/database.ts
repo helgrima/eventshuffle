0d102c9f70be2324741cad3fac9b5e2caa18d7b4
helgrima <arttu.tamminen@kapsi.fi>
2019-03-13 21:53:36 +0200

    initial data access layer, implemented event listing and event creation

--- src/models/database.ts
+++ src/models/database.ts
@@ -1,18 +1,47 @@
-import {Sequelize} from 'sequelize-typescript';
-import Event from "./event";
-import Participant from "./participant";
-import Vote from "./vote";
-import When from "./when";
+import { Sequelize } from 'sequelize-typescript';
+import { Event, EventCreate } from "./event";
+import { Participant } from "./participant";
+import { Vote } from "./vote";
+import { When } from "./when";
 
-export const Database = new Sequelize({
-    dialect: "sqlite",
-    username: "root",
-    password: "",
-    database: "eventshuffle",
-    storage: "eventshuffle.sqlite"
-});
+class Database {
+    context: Sequelize;
+    constructor() {
+        this.context = new Sequelize({
+            dialect: "sqlite",
+            username: "root",
+            password: "",
+            database: "eventshuffle",
+            storage: "eventshuffle.sqlite"
+        });
+        this.context.addModels([Event, Participant, Vote, When]);
+        //this.context.sync();
+    }
 
-Database.addModels([Event, Participant, Vote, When]);
-Database.sync();
+    async listEvents() : Promise<Event[]>  {
+        const events = await Event.findAll();
+        return events;
+    }
+
+    async createEvent(eventCreate : EventCreate) : Promise<Event> {
+        let newEvent = new Event({
+            name: eventCreate.name
+        });
+        const e = await newEvent.save().then((event) => {
+            for(let d in eventCreate.dates) {
+                let newWhen = new When({
+                    date: new Date(eventCreate.dates[d]),
+                    eventId: event.id
+                });
+                newWhen.save();
+            }
+            return event;
+        }).catch((error) => {
+            return error;
+        });
+        
+        return e;
+    }
+}
 
 export default Database;
